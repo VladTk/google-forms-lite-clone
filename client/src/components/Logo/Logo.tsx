4b28ca6e074123f65ab26c8f0da7e2da5ed8032a
Vlad Tkachuk <vlad.tkachuk.04@gmail.com>
2025-07-21 19:32:43 +0300

    fix: correct logo tag

--- client/src/components/Logo/Logo.tsx
+++ client/src/components/Logo/Logo.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import styles from './Logo.module.scss';
+import { Link } from 'react-router-dom';
 
 export const Logo: React.FC = () => {
   return (
-    <a href="/" className={styles.logo}>
+    <Link to="/" className={styles.logo}>
       <img
         src="/google-forms-logo.webp"
         alt="Google Forms Logo"
         className={styles.logo__image}
       />
       <span className={styles.logo__text}>Forms</span>
-    </a>
+    </Link>
   );
 };
