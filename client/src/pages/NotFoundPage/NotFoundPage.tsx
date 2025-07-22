0633ac7ab84e69fcef1049624afb621221794ea2
Vlad Tkachuk <vlad.tkachuk.04@gmail.com>
2025-07-22 14:08:20 +0300

    feat: implement 404 Not Found page

--- client/src/pages/NotFoundPage/NotFoundPage.tsx
+++ client/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,5 +1,31 @@
 import type React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { Container } from '../../components';
+import { Button } from '../../components/ui';
+
+import styles from './NotFoundPage.module.scss';
 
 export const NotFoundPage: React.FC = () => {
-  return <main></main>;
+  const navigate = useNavigate();
+
+  const handleGoHome = () => {
+    navigate('/');
+  };
+
+  return (
+    <main className={styles['not-found']}>
+      <Container>
+        <div className={styles['not-found__info']}>
+          <h1 className={styles['not-found__title']}>Page Not Found</h1>
+          <p className={styles['not-found__description']}>
+            Sorry, the page you're looking for doesn't exist.
+          </p>
+        </div>
+        <Button onClick={handleGoHome} className={styles['not-found__btn']}>
+          Go to Home
+        </Button>
+      </Container>
+    </main>
+  );
 };
